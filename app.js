b015ad3ef33032fdb8131a5426e3b835929755aa
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-04-13 16:08:24 -0400

    changes

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path');
-
-const app = express()
-const PORT = process.env.PORT || 3000 // Heroku will need the PORT environment variable
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}!`))
